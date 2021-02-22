2943bb4c4bb2d3ab73a262c59619da6265de92e6
user2021 <78174118+user2021@users.noreply.github.com>
2021-02-21 19:21:39 -0600

    Update Main.java

--- src/Loans_Packages/Main.java
+++ src/Loans_Packages/Main.java
@@ -1,21 +1,23 @@
 package Loans_Packages;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Map<List<Mortgage>, List<Document>> Loan_Contracts = new HashMap<>();
-
         List<Mortgage> houses = new ArrayList<>();
         List<Document> terms = new ArrayList<>();
+
+        Map<List<Mortgage>, List<Document>> Loan_Contracts = new HashMap<>();
 
         Mortgage mortgageObject0 = new Mortgage("WOOD", "15-YR");
         houses.add(mortgageObject0);
@@ -35,16 +37,37 @@ public class Main {
         Document loanObject3 = new Document("5D", "Interest_1.5%");
         terms.add(loanObject3);
 
-        
-        
         Loan_Contracts.put(houses, terms);
         System.out.println(Loan_Contracts);
 
         System.out.println();
         System.out.println();
 
+        Map<Mortgage, Document> Loan_Contracts_II = IntStream.range(0, houses.size())
+                .mapToObj(i -> Map.entry(houses.get(i), terms.get(i)))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 
-//        System.out.println();
+        Loan_Contracts_II.entrySet().stream()
+                .collect(Collectors.groupingBy(e -> e.getKey().getM_type(),
+                        Collectors.minBy(Comparator.comparing(e -> e.getValue().getM_rate()))
+                ))
+                .forEach((k, v) -> System.out.printf("type=%s, %s%n", k, v.map(Map.Entry::getValue).orElse(null)));
+
+        
+       Map<String,String> mapStringString = Loan_Contracts_II.entrySet().stream()
+                .collect(Collectors.groupingBy(e -> e.getKey().getM_type(),
+                        Collectors.minBy(Comparator.comparing(e -> e.getValue().getM_rate()))
+                )).put(key, e-> e.getValue().getM_rate());
+               
+//                .forEach((k, v) -> System.out.printf("type=%s, %s%n", k, v.map(Map.Entry::getValue).orElse(null)));
+ 
+        
+        
+//                .forEach((k, v) -> System.out.printf("type=%s, %s%n",k, v.map(Map.Entry::getValue).orElse(null)));
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
 //        Map<String, String> filteredHouses = Loan_Contracts.entrySet()
 //                .stream()
 //                .filter(entry -> entry.getValue())
